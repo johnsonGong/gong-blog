c664bc712faa5d822d95853d430e1c9b792d2b57
johnson <johnson@DESKTOP-88D9NCL>
2017-01-11 14:53:30 +0800

    代码格式化;

--- js/01_useEchart.js
+++ js/01_useEchart.js
@@ -3,27 +3,44 @@ console.log("use echartJS !");
 ;
 (function(window) {
 
-    var myChart = echarts.init(document.getElementById('chart_01'));
+    initChartBar();
+    
+    
+    
+    
+    function initChartBar(){
+        
+        var myChart_01 = echarts.init(document.getElementById('chart_01'));
 
-    var options = {
-        title: {
-            text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-            data: ['销量']
-        },
-        xAxis: {
-            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-        },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }]
+        var options = {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data: ['销量']
+            },
+            xAxis: {
+                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+    
+        myChart_01.setOption(options);
+        
     };
-
-    myChart.setOption(options);
+    
+    // 层次-树状结构.
+    function initChartForce_1() {
+        
+        var options = {
+            'type': "graph",
+        };
+    };
 
 }(window));
